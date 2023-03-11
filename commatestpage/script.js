ca50f8296d36242a56d3685e623b1328557f8539
JakeBritten <jacobbritten@outlook.com>
2023-03-11 17:34:16 -0500

    trying to get it to work without styles

--- commatestpage/script.js
+++ commatestpage/script.js
@@ -1,12 +1,15 @@
-let inputArray = []; // Initialize an empty array to hold input text
-function generateText() { // Define a function to generate comma-separated text
-  const input = document.getElementById("input-text").value; // Get the current input text
-  inputArray.push(input); // Add the input text to the input array
-  document.getElementById("input-text").value = ""; // Clear the input text field
-  const output = inputArray.join(", "); // Join the input array elements with commas
-  const outputContainer = document.getElementById("output-container"); // Get the output container element
-  const outputLine = document.createElement("p"); // Create a new paragraph element for the output text
-  outputLine.textContent = output; // Set the output text as the text content of the new paragraph element
-  outputContainer.innerHTML = ""; // Clear the previous output text
-  outputContainer.appendChild(outputLine); // Append the new paragraph element to the output container
+let inputArray = [];
+const generateBtn = document.getElementById("generate-btn");
+generateBtn.addEventListener("click", generateText);
+
+function generateText() {
+  const input = document.getElementById("input-text").value;
+  inputArray.push(input);
+  document.getElementById("input-text").value = "";
+  const output = inputArray.join(", ");
+  const outputContainer = document.getElementById("output-container");
+  const outputLine = document.createElement("p");
+  outputLine.textContent = output;
+  outputContainer.innerHTML = "";
+  outputContainer.appendChild(outputLine);
 }
